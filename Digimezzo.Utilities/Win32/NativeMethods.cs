2b4ae345b3efc4cd6d43ad427beef8af5fd72d8b
Raphaël <raphael.godart@outlook.com>
2017-01-01 18:25:19 +0100

    Fixed a bug calling SetWindowLongPtr on 32 bit systems

--- Digimezzo.Utilities/Win32/NativeMethods.cs
+++ Digimezzo.Utilities/Win32/NativeMethods.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Runtime.InteropServices;
 
 namespace Digimezzo.Utilities.Win32
@@ -11,13 +12,49 @@ namespace Digimezzo.Utilities.Win32
         [DllImport("user32.dll", SetLastError = true)]
         public static extern Int32 GetWindowLong(IntPtr hWnd, int nIndex);
 
-        [DllImport("user32.dll", EntryPoint = "SetWindowLongPtr", SetLastError = true)]
-        public static extern IntPtr SetWindowLongPtr(IntPtr hWnd, int nIndex, IntPtr dwNewLong);
-
         [DllImport("kernel32.dll", EntryPoint = "SetLastError")]
         public static extern void SetLastError(int dwErrorCode);
 
         [DllImport("user32.dll")]
         static internal extern int SetWindowCompositionAttribute(IntPtr hwnd, ref WindowCompositionAttributeData data);
+
+        /// <summary>
+        /// Due to a bug in User32.dll, calling SetWindowLongPtr fails on 32 bit systems. 
+        /// This exception occurs: Unable to find an entry point named 'SetWindowLongPtr' in DLL 'user32.dll'
+        /// See: https://www.medo64.com/2013/07/setwindowlongptr/
+        /// </summary>
+        /// <param name="hWnd"></param>
+        /// <param name="nIndex"></param>
+        /// <param name="dwNewLong"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll", SetLastError = true, EntryPoint = "SetWindowLong")]
+        [SuppressMessage("Microsoft.Portability", "CA1901:PInvokeDeclarationsShouldBePortable", MessageId = "return", Justification = "This declaration is not used on 64-bit Windows.")]
+        [SuppressMessage("Microsoft.Portability", "CA1901:PInvokeDeclarationsShouldBePortable", MessageId = "2", Justification = "This declaration is not used on 64-bit Windows.")]
+        public static extern IntPtr SetWindowLongPtr32(IntPtr hWnd, Int32 nIndex, IntPtr dwNewLong);
+
+        /// <summary>
+        /// Due to a bug in User32.dll, calling SetWindowLongPtr fails on 32 bit systems. 
+        /// This exception occurs: Unable to find an entry point named 'SetWindowLongPtr' in DLL 'user32.dll'
+        /// See: https://www.medo64.com/2013/07/setwindowlongptr/
+        /// </summary>
+        /// <param name="hWnd"></param>
+        /// <param name="nIndex"></param>
+        /// <param name="dwNewLong"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll", SetLastError = true, EntryPoint = "SetWindowLongPtr")]
+        [SuppressMessage("Microsoft.Interoperability", "CA1400:PInvokeEntryPointsShouldExist", Justification = "Entry point does exist on 64-bit Windows.")]
+        public static extern IntPtr SetWindowLongPtr64(IntPtr hWnd, Int32 nIndex, IntPtr dwNewLong);
+
+        public static IntPtr SetWindowLongPtr(IntPtr hWnd, int nIndex, IntPtr dwNewLong)
+        {
+            if (IntPtr.Size == 4)
+            {
+                return SetWindowLongPtr32(hWnd, nIndex, dwNewLong);
+            }
+            else
+            {
+                return SetWindowLongPtr64(hWnd, nIndex, dwNewLong);
+            }
+        }
     }
 }

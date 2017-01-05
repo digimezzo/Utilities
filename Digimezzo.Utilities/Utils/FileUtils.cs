e465538605961b303aaaa96897828348fea97880
PeoLeser <peoleser@gmail.com>
2017-01-05 21:38:33 +0900

    Add MoveFileToRecycleBin

--- Digimezzo.Utilities/Utils/FileUtils.cs
+++ Digimezzo.Utilities/Utils/FileUtils.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Text.RegularExpressions;
+using Digimezzo.Utilities.Win32;
 
 namespace Digimezzo.Utilities.Utils
 {
@@ -68,6 +69,48 @@ namespace Digimezzo.Utilities.Utils
             retVal = retVal.Replace("|", replaceStr);
 
             return retVal;
+        }
+
+        /// <summary>
+        /// Send file to recycle bin
+        /// </summary>
+        /// <param name="path">Location of directory or file to recycle</param>
+        /// <param name="flags">FileOperationFlags to add in addition to FOF_ALLOWUNDO</param>
+        public static bool Send(string path, FileOperationFlags flags)
+        {
+            try
+            {
+                var fs = new SHFILEOPSTRUCT
+                {
+                    wFunc = FileOperationType.FO_DELETE,
+                    pFrom = path + '\0' + '\0',
+                    fFlags = FileOperationFlags.FOF_ALLOWUNDO | flags
+                };
+                NativeMethods.SHFileOperation(ref fs);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Send file to recycle bin.  Display dialog, display warning if files are too big to fit (FOF_WANTNUKEWARNING)
+        /// </summary>
+        /// <param name="path">Location of directory or file to recycle</param>
+        public static bool Send(string path)
+        {
+            return Send(path, FileOperationFlags.FOF_NOCONFIRMATION | FileOperationFlags.FOF_WANTNUKEWARNING);
+        }
+
+        /// <summary>
+        /// Send file silently to recycle bin. Surpress dialog, surpress errors, delete if too large.
+        /// </summary>
+        /// <param name="path">Location of directory or file to recycle</param>
+        public static bool MoveToRecycleBin(string path)
+        {
+            return Send(path, FileOperationFlags.FOF_NOCONFIRMATION | FileOperationFlags.FOF_NOERRORUI | FileOperationFlags.FOF_SILENT);
         }
     }
 }

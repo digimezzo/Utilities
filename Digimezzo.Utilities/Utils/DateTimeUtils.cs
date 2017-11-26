4315ff2d78e5ea1e76caf325f9f8e4f0b1869110
Raphaël <raphael.godart@outlook.com>
2017-11-26 23:34:39 +0100

    Added a date validator

--- Digimezzo.Utilities/Utils/DateTimeUtils.cs
+++ Digimezzo.Utilities/Utils/DateTimeUtils.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Digimezzo.Utilities.Utils
 {
@@ -8,6 +9,18 @@ namespace Digimezzo.Utilities.Utils
         {
             DateTime referenceTime = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
             return (long)(dateTime.ToUniversalTime() - referenceTime).TotalSeconds;
+        }
+
+        public static bool IsValidDate(int year, int month, int day)
+        {
+            DateTime dateValue;
+
+            if (DateTime.TryParseExact($"{year}-{month.ToString("D2")}-{day.ToString("D2")}", "yyyy-MM-dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out dateValue))
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
